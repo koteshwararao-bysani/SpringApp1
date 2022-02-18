db19ddc3834554420faa3b8fb7fd39dfe7daee97
koteshwararao-bysani <koteshwararao.bysani@gmail.com>
2022-02-18 09:48:52 +0530

    kotesh committed second time

--- src/main/java/com/demo/SpringApp1/SpringApp1Application.java
+++ src/main/java/com/demo/SpringApp1/SpringApp1Application.java
@@ -9,6 +9,7 @@ public class SpringApp1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApp1Application.class, args);
 		System.out.println("i am from main ");
+		System.out.println("committed 2 time ");
 	}
 
 }

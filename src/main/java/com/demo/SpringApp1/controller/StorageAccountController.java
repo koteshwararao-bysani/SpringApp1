00a9c80193b3403a286d9dcb65e753c695017811
koteshwararao-bysani <koteshwararao.bysani@gmail.com>
2022-02-22 22:26:48 +0530

    changes commiteed by kotesh1

--- src/main/java/com/demo/SpringApp1/controller/StorageAccountController.java
+++ src/main/java/com/demo/SpringApp1/controller/StorageAccountController.java
@@ -2,9 +2,7 @@ package com.demo.SpringApp1.controller;
 
 import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.ListBlobItem;
+import com.microsoft.azure.storage.blob.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
@@ -22,6 +20,10 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.EnumSet;
 
 @RestController
 @RequestMapping("/storageaccountdemo")
@@ -69,9 +71,13 @@ public class StorageAccountController {
     @GetMapping("/download/imagefile")
     public ResponseEntity<ByteArrayResource> getImageFile() throws IOException, URISyntaxException, InvalidKeyException, StorageException {
         System.out.println("i am from StorageAccountController :: getImageFile()");
-        ListBlobItem blobList  = getCloudBlobContainer().listBlobs().iterator().next();
+
+        CloudBlobContainer cloudBlobContainerObj = this.getCloudBlobContainer();
+        String sasContainerToken = this.genContainerSASToken(cloudBlobContainerObj);
+        ListBlobItem blobList  = cloudBlobContainerObj.listBlobs().iterator().next();
         //ListBlobItem blobList = (ListBlobItem) getCloudBlobContainer().listBlobs();
-        Resource blobResource = new UrlResource(blobList.getUri());
+        //Resource blobResource = new UrlResource(blobList.getUri());
+        Resource blobResource = new UrlResource(blobList.getUri()+"?"+sasContainerToken);
         byte[] data = blobResource.getInputStream().readAllBytes();
         return ResponseEntity.ok()
                 .contentLength(data.length)
@@ -82,17 +88,34 @@ public class StorageAccountController {
 
 
     String connectionString = "DefaultEndpointsProtocol=https;AccountName=privatestorageaccount7;AccountKey=stogjQH7yZfz1KHrdeUOwrTVCTEejgx8NEPXaql1vD7iWUG+a4vasDVePq83Nmv2U6Cm8p4JmhqV+ASt8mBz2Q==;EndpointSuffix=core.windows.net";
+    //String connectionString = "BlobEndpoint=https://privatestorageaccount7.blob.core.windows.net/;QueueEndpoint=https://privatestorageaccount7.queue.core.windows.net/;FileEndpoint=https://privatestorageaccount7.file.core.windows.net/;TableEndpoint=https://privatestorageaccount7.table.core.windows.net/;SharedAccessSignature=sv=2020-08-04&ss=bfqt&srt=c&sp=rwdlacupitfx&se=2022-02-22T23:37:48Z&st=2022-02-22T15:37:48Z&spr=https&sig=JuNVBA9BBh5oEwxCSmxFiXmaRleIqzIajn14RMhOzdQ%3D";
     String containerName = "imagecontainer";
 
     public CloudBlobContainer getCloudBlobContainer() throws URISyntaxException, InvalidKeyException, StorageException {
         CloudBlobContainer cloudBlobContainer = null;
-
         CloudStorageAccount cloudStorageAccount;
         cloudStorageAccount = CloudStorageAccount.parse(connectionString);
         CloudBlobClient cloudBlobClient = cloudStorageAccount.createCloudBlobClient();
         cloudBlobContainer = cloudBlobClient.getContainerReference(containerName);
-
         return cloudBlobContainer;
+    }
+
+    public String genContainerSASToken(CloudBlobContainer cloudBlobContainer) throws InvalidKeyException, StorageException {
+        SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+        Date expirationDate = Date.from(Instant.now().plus(Duration.ofDays(1)));
+        sasConstraints.setSharedAccessExpiryTime(expirationDate);
+        EnumSet<SharedAccessBlobPermissions> permissions = EnumSet.of(
+                SharedAccessBlobPermissions.WRITE,
+                SharedAccessBlobPermissions.LIST,
+                SharedAccessBlobPermissions.READ,
+                SharedAccessBlobPermissions.DELETE);
+        sasConstraints.setPermissions(permissions);
+
+        //Generate the shared access signature on the container, setting the constraints directly on the signature.
+        String sasContainerToken = cloudBlobContainer.generateSharedAccessSignature(sasConstraints, null);
+        System.out.println("sasContainerToken :: "+sasContainerToken);
+
+      return  sasContainerToken;
     }
 
 }
